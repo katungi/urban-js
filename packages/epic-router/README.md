f9bbf25fdaa3d6a6a16974e23dd950ab09439c89
katungi <dankatdennis@gmail.com>
2023-12-21 23:45:12 +0300

    feat: created a filebased routing package

---
+++ packages/epic-router/README.md
@@ -0,0 +1,5 @@
+# EPIC ROUTER
+
+This is a simple router written for Epic framework.
+
+This router exposes a component `< Router />` that is wrapped around your project and allows file based routing.
---
+++ packages/epic-router/src/router.tsx
@@ -0,0 +1,45 @@
+import { Fragment, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+const ROUTES = import.meta.glob('/src/pages/**/[a-z[]*.{tsx,jsx}', { eager: true })
+// add preserved routes like _app.tsx and 404.tsx. 
+// TODO: add the default routes to the template, and write docs on the same.
+
+const PRESERVED = import.meta.glob('/src/pages/(_app|404).{tsx,jsx}', { eager: true })
+
+const routes = Object.keys(ROUTES).map((route) => {
+    const path = route
+        .replace(/\/src\/pages|index|\.tsx$/g, '')
+        .replace(/\[\.{3}.+\]/, '*')
+        .replace(/\[(.+)\]/, ':$1')
+
+    // @ts-ignore
+    return { path, component: ROUTES[route].default }
+})
+
+const preserved = Object.keys(PRESERVED).reduce((preserved, file) => {
+    const key = file.replace(/\/src\/pages\/|\.tsx$/g, '')
+    // @ts-ignore
+    return { ...preserved, [key]: PRESERVED[file].default }
+}, {})
+
+export default function Router() {
+    // Sorry for the wrong types. I'm still learning TS.
+    // @ts-ignore
+    const App = preserved?.['_app'] || Fragment
+    // @ts-ignore
+    const NotFound = preserved?.['404'] || Fragment
+
+    return (
+        <App>
+            <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                    {routes.map(({ path, component: Component = Fragment }) => (
+                        <Route key={path} path={path} element={<Component/>} />
+                    ))}
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Suspense>
+        </App>
+    )
+}

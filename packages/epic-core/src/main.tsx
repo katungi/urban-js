e9861540fa1e054829beb75a19d49d595a6f25dc
katungi <dankatdennis@gmail.com>
2023-12-22 01:42:04 +0300

    feat: created simple docs

--- packages/epic-core/src/main.tsx
+++ packages/epic-core/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Router as EpicRouter } from './router'
+
+// migrate to this when the below is fixed
 // import { Router as EpicRouter } from 'epic-router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

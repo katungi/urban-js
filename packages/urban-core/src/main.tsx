49ccf1e9a50dd3d73226b1062bd95a5e8357c1b5
katungi <dankatdennis@gmail.com>
2024-05-18 15:43:07 +0300

    fix: fixed urban router import bug

--- packages/urban-core/src/main.tsx
+++ packages/urban-core/src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Router as UrbanRouter } from '../utils/router'
 
 // migrate to this when the below is fixed
-// import { Router as UrbanRouter } from 'urban-router'
+import { UrbanRouter } from 'urban-router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-    <Router>
-      <UrbanRouter />
-    </Router>
-
+    <UrbanRouter />
   </React.StrictMode>,
 )
---
+++ packages/urban-core/src/pages/products.tsx
@@ -0,0 +1,8 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <a href="/">Home</a>
+        </div>
+    )
+}

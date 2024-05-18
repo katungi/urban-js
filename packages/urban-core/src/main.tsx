30337980b400a9407115edd26f840f8bad138986
katungi <dankatdennis@gmail.com>
2024-05-18 16:21:07 +0300

    feat: actually* fixed the routing import bug

--- packages/urban-core/src/main.tsx
+++ packages/urban-core/src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
 // migrate to this when the below is fixed
 import { UrbanRouter } from 'urban-router'
 
+// TODO: Find a way to export a glob mechanism from the router package
+const Routes = import.meta.glob('/src/pages/[**/a-z[]*.{tsx,jsx}', { eager: true });
+const PRESERVED = import.meta.glob('/src/pages/(_app|404|_document).{tsx,jsx}', { eager: true });
+
+console.log(PRESERVED)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UrbanRouter />
+    <BrowserRouter>
+      <UrbanRouter ROUTES={Routes} PRESERVED={PRESERVED} />
+    </BrowserRouter>
   </React.StrictMode>,
 )
